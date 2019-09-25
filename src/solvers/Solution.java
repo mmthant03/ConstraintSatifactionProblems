9ff6ffec2ef69d9c7d8ccbc6ff5629697dcfcf67
Robert Dutile <bdbooksrule@gmail.com>
2019-09-25 14:29:01 -0400

    Solution class

---
+++ src/solvers/Solution.java
@@ -0,0 +1,29 @@
+package solvers;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import ItemBag.Constraint;
+
+//Robert Dutile & Myo Min Thant
+public class Solution {
+	
+	public HashMap<Character, ArrayList<Character>> bagContents;
+	public boolean isFailure;
+	
+	//constructor
+	public Solution() {
+		this.bagContents = new HashMap<>();
+		isFailure = false;
+	}
+	
+	public HashMap<Character, ArrayList<Character>> getMap() {
+		return bagContents;
+	}
+	
+	public void setFailure() {
+		isFailure = true;
+	}
+	
+	
+
+}
